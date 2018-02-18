02dc94050f3f73c793dea287c3c216c4a76a7586
Greg Perrin <greg.perrin@mfactorengineering.com>
2018-02-17 20:49:35 -0700

    Web3 wrapper, custodian view and routes.

--- custodian/Custodian.js
+++ custodian/Custodian.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Header, Title, Left, Icon, Right, Button, Body, Content,Text, Card, CardItem, H1 } from "native-base";
 
-
 export default class Custodian extends Component {
   render() {
     return (
--- lib3.js
+++ lib3.js
@@ -1,33 +1,36 @@
 import Web3 from 'web3';
-// import { Web3Wrapper } from '@0xproject/web3-wrapper';
+import CustodyToken from './build/contracts/CustodyToken.json';
+
+const custodyAddr = '0x7ba431d1f33cb44ebd5d2f6ba541e9ff6902120e';
 
 class Web3Wrapper {
   constructor(provider) {
     // (provider as any).sendAsync = (provider as any).send;
     this.web3 = new Web3();
     this.web3.setProvider(provider);
+    this.contractInstance = null;
+
+    this.getContractFromAbi();
   }
 
   isAddress(address) {
     return this.web3.isAddress(address);
   }
 
-  getContractFromAbi(abi) {
-    const web3Contract = this.web3.eth.contract(abi);
-    return web3Contract;
+  getContractFromAbi() {
+    this.contractInstance = new this.web3.eth.Contract(CustodyToken.abi, custodyAddr);
   }
 
-  getContractInstance(abi, address) {
-    const web3ContractInstance = this.getContractFromAbi(abi).at(address);
-    return web3ContractInstance;
-  }
-
-  async sendTransactionAsync(txData) {
-    const txHash = await (this.web3.eth.sendTransaction)(txData);
-    return txHash;
+  async contractFunc(funcName, isMutable, opts) {
+    if (isMutable) {
+      return await this.contractInstance[funcName]().call();
+    }
+    // ...send(arg, { from: someAddr, to: someAddr, gas: someGas })
+    return await this.contractInstance[funcName]().send(opts);
   }
 }
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://iris.local:8545'));
 const web3Wrapper = new Web3Wrapper(web3.currentProvider);
+
 export default web3Wrapper;
